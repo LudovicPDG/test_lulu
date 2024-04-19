cd90d93781161bb63bae19a4441271d1b5cb9d71
Ludovic <ludovic.pons-de-guigues@etu.univ-amu.fr>
2024-04-19 09:58:29 +0200

    Add initial project files and dependencies

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::process;
+
+use test_lulu::Config;
+use test_lulu::run;
+
+
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
